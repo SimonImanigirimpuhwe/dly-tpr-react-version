3588bfb3276a9f18b33305d54dc9e7cd8c170dec
SimonImanigirimpuhwe <softimag08@gmail.com>
2020-12-25 14:51:45 +0200

    chore(context): set up context for state management

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 import Admin from './components/Admin';
+import AuthContextProvider from './context/contexts/AuthContext';
+
 
 const App = () => {
     const {pathname} = window.location;
     const exceptAdmin = pathname !== '/staff'
     return ( 
-        <Router>
-        {exceptAdmin && <Header />}
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/staff" component={Admin} />
-        </Switch>
-        {exceptAdmin && <Footer />}
-        </Router>
+        <AuthContextProvider>
+            <Router>
+            {exceptAdmin && <Header />}
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/staff" component={Admin} />
+            </Switch>
+            {exceptAdmin && <Footer />}
+            </Router>
+        </AuthContextProvider>
      );
 }
 
